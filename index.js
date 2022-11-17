b8661b02a7137e37a5849ce820fa792ea8e6ca4c
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-17 14:45:31 -0600

    created the functions for the basic math operations

---
+++ index.js
@@ -0,0 +1,20 @@
+let a = 0;
+let b = 0;
+
+function add() {
+  return a + b;
+}
+
+function subtract() {
+    return a - b;
+}
+
+function multiply() {
+    return a * b;
+}
+
+function divide() {
+    return a / b;
+}
+
+
