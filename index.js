d7b95161b4c3a52f249cfc4614fa0f00f6098cc1
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-17 14:52:32 -0600

    added the values to the buttons and trying to populate the screen with the user input

--- index.js
+++ index.js
@@ -17,4 +17,39 @@ function divide() {
     return a / b;
 }
 
+/* Create a new function operate that takes an operator and 2 numbers 
+and then calls one of the above functions on the numbers.*/
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
+/* Create the functions that populate the display when you 
+click the number buttons. You should be storing the 
+‘display value’ in a variable somewhere for use 
+in the next step. */
+
+function displayNumber() {
+    const display = document.querySelector('.display');
+    display.textContent = a;
+}
+
+function displayNumber2() {
+    const display = document.querySelector('.display');
+    display.textContent = b;
+}
+
+document.querySelector('.one').addEventListener('click', () => {
+    a = 1;
+    displayNumber();
+}
+
 
