7d79793500a6db67d510359daabf5899d507dac4
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-18 01:05:18 -0600

    cleaned up the code a little

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let a = 0;
 let b = 0;
+let operation = '';
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+const memory = document.querySelector('.memory');
+const current = document.querySelector('.current');
+const firstArray = [];
+const secondArray = [];
 
+// the math functions
 function add() {
   return a + b;
 }
@@ -19,7 +27,6 @@ function divide() {
 
 /* Create a new function operate that takes an operator and 2 numbers 
 and then calls one of the above functions on the numbers.*/
-
 function operate(operator, a, b) {
     if (operator === '+') {
         return add(a, b);
@@ -36,17 +43,8 @@ function operate(operator, a, b) {
 click the number buttons. You should be storing the 
 ‘display value’ in a variable somewhere for use 
 in the next step. */
-
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display');
-const memory = document.querySelector('.memory');
-const current = document.querySelector('.current');
-
-const firstArray = [];
-const secondArray = [];
-
-//creat a fuction that takes user and populates and array with the numbers diplays them on the screen
-
+/*creat a fuction that takes user and populates and array with 
+the numbers diplays them on the screen/ */
 function populateDisplay() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -58,12 +56,8 @@ function populateDisplay() {
     });
 }
 
-populateDisplay();
-
 // a functin that listens for the operator button to be clicked. on click it converts the array to a number and
 // stores it in a variable.
-let operation = '';
-
 function operator() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -82,12 +76,9 @@ function operator() {
     });
 }
 
-operator();
-
 // a function that listens for the equals button to be clicked. on click it converts the array to a number and
 // stores it in a variable. it then calls the operate function and passes the operator and the two numbers
 // as arguments. it then displays the result on the screen.
-
 function equals() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -102,9 +93,8 @@ function equals() {
     });
 }
 
-equals();
-
-// a function that listens for the clear button to be clicked. on click it clears the display and the memory
+/* a function that listens for the clear button to be clicked. 
+on click it clears the display and the memory*/
 
 function clear() {
     buttons.forEach((button) => {
@@ -119,10 +109,7 @@ function clear() {
     });
 }
 
-clear();
-
 // a function that listens for the backspace button to be clicked. on click it removes the last number from the display
-
 function backspace() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -134,10 +121,7 @@ function backspace() {
     });
 }
 
-backspace();
-
 // a function that listens for the decimal button to be clicked. on click it adds a decimal to the display
-
 function decimal() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -149,8 +133,9 @@ function decimal() {
     });
 }
 
+populateDisplay();
+operator();
+equals();
+clear();
+backspace();
 decimal();
-
-
-
-
